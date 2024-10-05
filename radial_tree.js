d8b713e370be9a17c4b0f1d34b134e1cb04b2a94
TanJiunKoon <jtan0374@student.monash.edu>
2024-10-05 12:52:23 +0800

    push

--- radial_tree.js
+++ radial_tree.js
@@ -1,28 +1,4 @@
-import { dataValues } from 'https://raw.githubusercontent.com/TanJiunKoon/FIT3179-HW10/refs/heads/main/data.js'; // Import dataset
-
-// Build a lookup map of id to node
-var dataMap = {};
-dataValues.forEach(function(node) {
-  dataMap[node.id] = node;
-});
-
-// Function to compute the path for each node
-dataValues.forEach(function(node) {
-  var pathNames = [node.name];
-  var parent = node.parent;
-  while (parent) {
-    var parentNode = dataMap[parent];
-    if (parentNode) {
-      pathNames.unshift(parentNode.name);
-      parent = parentNode.parent;
-    } else {
-      break;
-    }
-  }
-  node.path = pathNames.join(' > ');
-});
-
-var spec = {
+{
   "$schema": "https://vega.github.io/schema/vega/v5.json",
   "description": "Radial Tree Layout of Economic Sectors with Tooltips",
   "width": 1080,
@@ -47,7 +23,7 @@ var spec = {
   "data": [
     {
       "name": "tree",
-      "values": dataValues,
+      "url": "https://raw.githubusercontent.com/TanJiunKoon/FIT3179-HW10/main/data.json",
       "transform": [
         { "type": "stratify", "key": "id", "parentKey": "parent" },
         {
@@ -134,8 +110,4 @@ var spec = {
       }
     }
   ]
-};
-
-vegaEmbed('#view', spec).then(function(result) {
-  // Access the Vega view instance
-}).catch(console.error);
+}
